2128a8801a02836cf639e5ad9c7535851e3942dd
R4DM4N <47617496+R4DM4N@users.noreply.github.com>
2025-10-30 17:25:22 +1000

    pygame boiler plate setup

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
 
 def main():
+    # inintialize pygame and game components
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
     print("Starting Asteroids!")
     print("Screen width: 1280")
     print("Screen height: 720")
     #print("Screen width: ", SCREEN_WIDTH)
     #print("Screen height: ", SCREEN_HEIGHT)	
-    
+     
+    # begin infinite game loop    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return
+
+    screen.fill(BLACK)
+    pygame.display.flip() 
+        
+
 if __name__ == "__main__":
     main()
