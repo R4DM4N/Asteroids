f55e3f0d12eea9d4d2a4162327127040324c97df
R4DM4N <47617496+R4DM4N@users.noreply.github.com>
2025-10-30 15:34:08 +1000

    Added constants.py

--- main.py
+++ main.py
@@ -2,10 +2,14 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
-    print("Starting asteroids!")
-
-
+    print("Starting Asteroids!")
+    print("Screen width: 1280")
+    print("Screen height: 720")
+    #print("Screen width: ", SCREEN_WIDTH)
+    #print("Screen height: ", SCREEN_HEIGHT)	
+    
 if __name__ == "__main__":
     main()
