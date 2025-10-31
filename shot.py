f5764ef6f35806d1aa5bd7c7688154ddfdf8d60f
R4DM4N <47617496+R4DM4N@users.noreply.github.com>
2025-10-31 18:56:34 +1000

    updated line width constans for asteroids and shot

--- shot.py
+++ shot.py
@@ -6,10 +6,10 @@ class Shot(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, SHOT_RADIUS)
         pygame.sprite.Sprite.__init__(self, self.containers)
-        
+
     def draw(self, screen):
         # code taken from asteroid.py
-        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
+        pygame.draw.circle(screen, "white", self.position, self.radius, SHOT_LINE_WIDTH)
     
     def update(self, dt):
         self.position += self.velocity * dt
