89a8fd10305cde627d16b6cfca231381ee5aa71f
R4DM4N <47617496+R4DM4N@users.noreply.github.com>
2025-10-31 11:04:31 +1000

    Sprite group containers used for group updates Player.containers = (updatable, drawable)

--- main.py
+++ main.py
@@ -12,39 +12,41 @@ def main():
     print("Starting Asteroids!")
     print("Screen width: ", SCREEN_WIDTH)
     print("Screen height: ", SCREEN_HEIGHT)
-    print(type(Player))
-    #SPAWN IN PLAYER
-    player = Player(SCREEN_WIDTH /2, SCREEN_HEIGHT /2, PLAYER_RADIUS)
-    
-    # debug
 
-    if player is not None:
-        player.draw(screen)
-    else:
-        raise Exception("Player has not been initialized!")
+    # Group containers
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    # Create all payer objects after this change
+    Player.containers = (updatable, drawable)
+
+    #SPAWN IN PLAYER
+    player = Player(SCREEN_WIDTH /2, SCREEN_HEIGHT /2, PLAYER_RADIUS)  
 	
-    # init delta time fps counter /1000 conrt to s
+    # Init delta time fps counter /1000 conrt to s
     dt = 0     
 
-    # begin infinite game loop
+    # Begin infinite game loop
     running = True
     while running: 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+        
         # Clear the screen and set the screen background
         screen.fill("black")        
 
-        # Move Player
-        player.update(dt)
-        # Draw Player       
-        player.draw(screen)
+        # Update all sprites
+        updatable.update(dt)
 
-
+       
+        # Draw objects
+        #drawable.draw(screen) Works on sprites only not vectors
+        for unit in drawable:
+            unit.draw(screen)
 
 
         # Go ahead and update the screen with what we've drawn.
-        # This MUST happen after all the other drawing commands.
         pygame.display.flip() 
         
         # Cap the frame rate
