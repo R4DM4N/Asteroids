cc1d0ba1bb69ddb0b9f96087f67503a687027b54
R4DM4N <47617496+R4DM4N@users.noreply.github.com>
2025-10-31 11:59:35 +1000

    added asteroid.py

---
+++ asteroid.py
@@ -0,0 +1,17 @@
+import pygame
+from circleshape import CircleShape
+from constants import *
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        # sub-classes must override
+        pygame.draw.circle(1,"white", self.position, self.radius, ASTEROID_LINE_WIDTH)
+
+    def rotate(self, dt):
+        pass
+
+    def update(self, dt):
+        self.velocity *= dt
