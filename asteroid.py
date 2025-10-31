64e062a1bc93687982e36dfcc7fa2a3398628696
R4DM4N <47617496+R4DM4N@users.noreply.github.com>
2025-10-31 12:54:04 +1000

    Boots helping, asteroids not visable updated draw by creating a centre tuple first and then passing it to pygame...... still nothing

--- asteroid.py
+++ asteroid.py
@@ -8,10 +8,11 @@ class Asteroid(CircleShape):
 
     def draw(self, screen):
         # sub-classes must override
-        pygame.draw.circle(screen, "white", self.position, self.radius, ASTEROID_LINE_WIDTH)
-
-    def rotate(self, dt):
-        pass
-
+        #pygame.draw.circle(screen, "white", self.position, self.radius, ASTEROID_LINE_WIDTH)
+        
+        # boots solution pygame needs a tuple of ints.
+        center = (int(self.position.x), int(self.position.y))
+        pygame.draw.circle(screen, "white", center, int(self.radius), ASTEROID_LINE_WIDTH)
+    
     def update(self, dt):
         self.velocity += self.velocity * dt
